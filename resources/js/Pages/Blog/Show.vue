255de231c4f7e3b4582d9a4b89a8e465dd5e9f5d
Valerijs Krizevics <kvikaramba@gmail.com>
2025-03-21 22:37:22 +0200

    adminpanelpages Layout applied to blog post show view

--- resources/js/Pages/Blog/Show.vue
+++ resources/js/Pages/Blog/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { defineProps } from 'vue';
 import { Link } from '@inertiajs/vue3';
+import Layout from '@/Layouts/AppLayout.vue';
 
 const props = defineProps({
   blogPost: Object,
@@ -9,24 +10,24 @@ const props = defineProps({
 
 <template>
   <Layout title="View Blog Post">
-    <div class="p-6">
-      <div class="mb-4">
-        <h1 class="text-2xl font-semibold">{{ blogPost.title }}</h1>
-        <p class="text-gray-500">By {{ blogPost.user?.name }} on {{ blogPost.created_at.split('T')[0] }}</p>
-      </div>
+    <div class="bg-white min-h-screen">
+      <div class="max-w-3xl mx-auto p-6 bg-white">
+        <h1 class="text-2xl font-semibold mb-4 sm-ml-6">{{ blogPost.title }}</h1>
+        <p class="text-gray-500 sm-ml-6">By {{ blogPost.user?.name }} on {{ blogPost.created_at.split('T')[0] }}</p>
+      
+        <div class="mb-4 sm-ml-6">
+          <p><em>{{ blogPost.short_description }}</em></p>
+        </div>
 
-      <div class="mb-4">
-        <p><em>{{ blogPost.short_description }}</em></p>
-      </div>
+        <div class="mb-4 sm-ml-6">
+          <p>{{ blogPost.text }}</p>
+        </div>
 
-      <div class="mb-4">
-        <p>{{ blogPost.text }}</p>
-      </div>
-
-      <div class="flex justify-end mt-4">
-        <Link :href="route('blog.index')" class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
-          Back
-        </Link>
+        <div class="flex justify-start mt-4 sm-ml-6">
+          <Link :href="route('blog.index')" class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
+            Back
+          </Link>
+        </div>
       </div>
     </div>
   </Layout>
