54babd90c373f13abfcc6b70f28b1627a9f9719b
Valerijs Krizevics <kvikaramba@gmail.com>
2025-03-22 18:07:21 +0200

    adminpanelpages Landing tweaks

--- resources/js/Pages/Landing/BlogIndex.vue
+++ resources/js/Pages/Landing/BlogIndex.vue
@@ -44,13 +44,23 @@ defineProps({
     </div>
 
     <h1 class="text-3xl font-bold mb-6">From the blogs</h1>
+    
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <div v-for="post in posts.data" :key="post.id" class="bg-white rounded-lg shadow-lg p-4">
-        <img :src="'/photos/' + (post.id % 10) + '.jpg'" class="rounded-md w-full h-40 object-cover">
-        <h2 class="mt-3 text-lg font-semibold">{{ post.title }}</h2>
-        <p class="text-sm text-gray-500">By {{ post.user.name }} • {{ new Date(post.created_at).toLocaleDateString('ru-RU') }}</p>
-        <p class="mt-2 text-gray-700">{{ post.short_description }}...</p>
-      </div>
+      
+        <div v-for="post in posts.data" :key="post.id" class="bg-white rounded-lg shadow-lg p-4">
+        
+          <img :src="'/photos/' + (post.id % 10) + '.jpg'" class="rounded-md w-full h-40 object-cover">
+          
+          <h2 class="mt-3 text-lg font-semibold">{{ post.title }}</h2>
+          
+          <p class="text-sm text-gray-500">
+            <strong>{{ post.user.name }}</strong> • {{ new Date(post.created_at).toLocaleDateString('ru-RU') }}
+          </p>
+          
+          <p class="mt-2 text-gray-700">{{ post.short_description }}</p>
+        
+        </div>
+
     </div>
   </div>
 </template>
