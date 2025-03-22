d6624bfaa5a3d56cc150a5c8fb8e68510e1ad6e8
Valerijs Krizevics <kvikaramba@gmail.com>
2025-03-22 11:37:51 +0200

    adminpanelpages Breadcrumbs code indentation fixed

--- resources/js/Components/Breadcrumbs.vue
+++ resources/js/Components/Breadcrumbs.vue
@@ -10,17 +10,17 @@ defineProps({
 </script>
 
 <template>
-    <nav class="text-sm text-gray-600 mb-4">
-      <ul class="flex">
-        <li v-for="(item, index) in items" :key="index">
-          <span v-if="index < items.length - 1">
-            <Link :href="item.link" class="hover:text-blue-600">{{ item.label }}</Link>
-            <span class="mx-2">/</span>
-          </span>
-          <span v-else>
-            {{ item.label }}
-          </span>
-        </li>
-      </ul>
-    </nav>
-  </template>
+  <nav class="text-sm text-gray-600 mb-4">
+    <ul class="flex">
+      <li v-for="(item, index) in items" :key="index">
+        <span v-if="index < items.length - 1">
+          <Link :href="item.link" class="hover:text-blue-600">{{ item.label }}</Link>
+          <span class="mx-2">/</span>
+        </span>
+        <span v-else>
+          {{ item.label }}
+        </span>
+      </li>
+    </ul>
+  </nav>
+</template>
